Fix NaN scaling when all target weights are higher than current ones

qubosolver/algorithms/blade/_distances_constraints_calculator.py
@@ -54,13 +54,18 @@ def compute_best_scaling_for_qubo(
         plt.show()
 
     if filter_differences:
-        embedded_qubo_triu = target_qubo_triu + limited_differences
+        # when no visible differences, use the input value to avoid rounding issues
+        filtered_embedded_qubo_triu = np.where(
+            differences == limited_differences,
+            embedded_qubo_triu,
+            target_qubo_triu + limited_differences,
+        )
 
     best_scaling = (
-        np.sum(embedded_qubo_triu**2) / np.sum(embedded_qubo_triu * target_qubo_triu)
+        np.sum(filtered_embedded_qubo_triu**2) / np.sum(filtered_embedded_qubo_triu * target_qubo_triu)
     ) ** (1 / 6)
 
-    assert not np.isnan(best_scaling)
+    assert not np.isnan(best_scaling) and not np.isinf(best_scaling)
 
     return best_scaling
 