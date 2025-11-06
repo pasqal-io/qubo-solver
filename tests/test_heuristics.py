54bda1d36b71f74b90b92d679a9589d59d2cbf24
Quentin Ma <quentin.ma@pasqal.com>
2025-11-06 10:32:09 +0100

    update sa test

--- tests/test_heuristics.py
+++ tests/test_heuristics.py
@@ -14,9 +14,12 @@ from qubosolver.classical_solver.classical_solver import (
 )
 
 
-def test_qubo_solver_sa(simple_qubo_instance: QUBOInstance) -> None:
+@pytest.mark.parametrize("sa_top_k", [1, 3])
+def test_qubo_solver_sa(simple_qubo_instance: QUBOInstance, sa_top_k: int) -> None:
     # Create a SolverConfig object with classical solver options.
-    classical_config = ClassicalConfig(classical_solver_type="simulated_annealing", sa_top_k=1)
+    classical_config = ClassicalConfig(
+        classical_solver_type="simulated_annealing", sa_top_k=sa_top_k
+    )
     config = SolverConfig(
         use_quantum=False, classical=classical_config, activate_trivial_solutions=False
     )
@@ -38,23 +41,8 @@ def test_qubo_solver_sa(simple_qubo_instance: QUBOInstance) -> None:
     # The optimal value for binary variables is achieved when both are 0,
     # so expect a cost of 0.
     # Also, check that the bitstring has the expected shape, e.g., [1,2].
-    assert solution.bitstrings.shape[0] == 1  # one solution returned
+    assert solution.bitstrings.shape[0] == sa_top_k  # one solution returned
     assert solution.bitstrings.shape[1] == 3  # three variables
-
-
-def test_qubo_solver_topk_sa(simple_qubo_instance: QUBOInstance) -> None:
-    classical_config = ClassicalConfig(classical_solver_type="simulated_annealing", sa_top_k=3)
-    config = SolverConfig(
-        use_quantum=False, classical=classical_config, activate_trivial_solutions=False
-    )
-    assert isinstance(
-        get_classical_solver(simple_qubo_instance, config.classical), SimulatedAnnealingSolver
-    )
-    solver = QuboSolver(simple_qubo_instance, config)
-    solution = solver.solve()
-    assert isinstance(solution, QUBOSolution)
-    assert solution.bitstrings.shape[0] == 3
-    assert solution.bitstrings.shape[1] == 3
 
 
 def test_qubo_solver_tabu() -> None:
