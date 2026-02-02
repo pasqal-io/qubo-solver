c37f46869d2fa238ed27ba6ac5d44a8d9bc1deb7
Antoine Bussy <antoine.bussy@pasqal.com>
2026-02-02 16:41:34 +0100

    [Typing] Introduce Concepts and Structural Typing

---
+++ qubosolver/concepts/backend.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from typing import Protocol, Sequence, Self, runtime_checkable
+from qoolqit import QuantumProgram
+from pulser.backend import Results
+
+class Backend(Protocol):
+
+    def run(self: Self, program: QuantumProgram) -> Sequence[Results]:
+        ...
---
+++ tests/concepts/test_concepts.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from qubosolver import concepts
+
+from typing import Sequence, Self
+from qoolqit import QuantumProgram
+from pulser.backend import Results
+
+# This "test" is intended to be run with mypy, not mypy
+# We use # type: ignore[...] in combination with the mypy flag "--warn-unused-ignores"
+# to detect whether a class matches the concept when it's expected not to.
+
+def _test_backend(backend: concepts.Backend) -> None:
+    ...
+
+def test_not_backend() -> None:
+
+    class NotBackend0:
+        ...
+    _test_backend(NotBackend0()) # type: ignore[arg-type]
+    b0: concepts.Backend = NotBackend0() # type: ignore[assignment]
+
+    class NotBackend1:
+        def run(self: Self, program: QuantumProgram) -> None:
+            return
+    _test_backend(NotBackend1()) # type: ignore[arg-type]
+    b1: concepts.Backend = NotBackend1() # type: ignore[assignment]
+
+    class NotBackend2:
+        def run(self: Self, program: int) -> Sequence[Results]:
+            return [Results(atom_order=(), total_duration=0)]
+    _test_backend(NotBackend2()) # type: ignore[arg-type]
+    b2: concepts.Backend = NotBackend2() # type: ignore[assignment]
+
+    class NotBackend3:
+        def run(self: Self, program: QuantumProgram) -> Sequence[Results] | None:
+            return [Results(atom_order=(), total_duration=0)]
+    _test_backend(NotBackend3()) # type: ignore[arg-type]
+    b3: concepts.Backend = NotBackend3() # type: ignore[assignment]
+
+def test_backend() -> None:
+
+    class Backend0:
+        def run(self: Self, program: QuantumProgram) -> Sequence[Results]:
+            return [Results(atom_order=(), total_duration=0)]
+
+    _test_backend(Backend0())
+    b0: concepts.Backend = Backend0()
+
+    class Backend1:
+        def run(self: Self, program: QuantumProgram | None) -> Sequence[Results]:
+            return [Results(atom_order=(), total_duration=0)]
+    _test_backend(Backend1())
+    b1: concepts.Backend = Backend1()
