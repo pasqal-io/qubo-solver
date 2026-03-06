fb3041464d22271f5918de92b3f53a0d3aefc5c8
Antoine Bussy <antoine.bussy@pasqal.com>
2026-03-06 13:10:59 +0100

    [Tests] Make tests more robust

--- tests/embeddings/test_greedy.py
+++ tests/embeddings/test_greedy.py
@@ -27,13 +27,39 @@ def triangular_qubo() -> torch.Tensor:
 def square_qubo() -> torch.Tensor:
     return torch.tensor(
         [
-            [0.0, 1.0, 1.0/8.0, 1.0],
-            [1.0, 0.0, 1.0, 1.0/8.0],
-            [1.0/8.0, 1.0, 0.0, 1.0],
-            [1.0, 1.0/8.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0 / 8.0, 1.0],
+            [1.0, 0.0, 1.0, 1.0 / 8.0],
+            [1.0 / 8.0, 1.0, 0.0, 1.0],
+            [1.0, 1.0 / 8.0, 1.0, 0.0],
         ],
         dtype=torch.float32,
     )
+
+
+def assert_close_up_to_isometry(
+    actual_vertices: torch.Tensor, expected_vertices: torch.Tensor, layout_angle: float
+) -> None:
+    A = torch.linalg.lstsq(expected_vertices, actual_vertices).solution
+    # A has a symmetry. Apply a symmetry to get a pure rotation.
+    if torch.linalg.det(A) < 0.0:
+        A = torch.diag(torch.tensor([-1.0, 1.0])) @ A
+
+    # A is a rotation matrix
+    torch.testing.assert_close(A.T @ A, torch.eye(2))
+    check.almost_equal(torch.linalg.det(A), 1.0)
+
+    # Triangular Layout has a pi/3 rotation invariance
+    # Square Layout has a pi/2 rotation invariance
+    # The angle of rotation should be an integer multiple of it
+    angle = torch.atan2(A[1, 0], A[0, 0]).item()
+    normalized_angle = angle / layout_angle
+    check.almost_equal(normalized_angle, round(normalized_angle))
+
+
+def assert_close_to_lattice(vertices: torch.Tensor, basis: torch.Tensor) -> None:
+    for v in vertices:
+        v_lattice = basis.inverse() @ v
+        torch.testing.assert_close(v_lattice, v_lattice.round())
 
 
 def interaction_matrix_from_vertices(vertices: torch.Tensor) -> torch.Tensor:
@@ -46,8 +72,13 @@ def interaction_matrix_from_vertices(vertices: torch.Tensor) -> torch.Tensor:
     return U
 
 
-@pytest.mark.parametrize("traps", [3, 6])
+@pytest.mark.parametrize("traps", [1, 2, 3, 6])
 def test_triangular_qubo(traps: int) -> None:
+    if traps <= 2:
+        # Raises "RuntimeError: Could not infer dtype of NoneType"
+        # Should either augment the number of traps automatically,
+        # or raise a meaningful error
+        pytest.xfail(reason="Bug to be fixed")
 
     spacing = 7.0
 
@@ -67,7 +98,8 @@ def test_triangular_qubo(traps: int) -> None:
             [0.0, 0.0],
             [0.5, 0.5 * np.sqrt(3)],
             [1.0, 0.0],
-        ]
+        ],
+        dtype=torch.float32,
     )
     #  Matrix Q should match the spacing of the triangular layout so that the embedding returns
     # an equilateral triangle, hence the scale alpha.
@@ -79,18 +111,33 @@ def test_triangular_qubo(traps: int) -> None:
 
     result = Greedy().launch_greedy(Q=Q, params=parameters)
     vertices = result[0][1]["coords"]
-    # Vertices form an equilateral triangle of side = spacing
-    for i in range(3):
-        for j in range(i + 1, 3):
-            d = torch.dist(vertices[i, :], vertices[j, :])
-            check.almost_equal(d, spacing)
+
+    assert_close_up_to_isometry(vertices, expected_vertices, torch.pi / 3.0)
+    # fmt: off
+    basis = spacing * torch.tensor(
+        [
+            [1.0, 0.0],
+            [0.5, np.sqrt(3) / 2.0],
+        ],
+        dtype=torch.float32,
+    ).T
+    # fmt: on
+    assert_close_to_lattice(vertices, basis)
 
     U = C6 * interaction_matrix_from_vertices(vertices)
     torch.testing.assert_close(U, expected_U)
 
-@pytest.mark.parametrize("traps", [4, 9])
+
+@pytest.mark.parametrize("traps", [1, 2, 4, 9])
 @pytest.mark.parametrize("layout", [LayoutType.SQUARE, "square"])
 def test_square_qubo(traps: int, layout: LayoutType | str) -> None:
+    # With 2 traps, greedy increases the number of traps automatically, but not with 1
+    # Is it desired ? Or should it raise a meaningful error ?
+    if traps == 1:
+        # Raises "RuntimeError: Could not infer dtype of NoneType"
+        # Should either augment the number of traps automatically,
+        # or raise a meaningful error
+        pytest.xfail(reason="Bug to be fixed")
 
     spacing = 7.0
 
@@ -104,7 +151,7 @@ def test_square_qubo(traps: int, layout: LayoutType | str) -> None:
         "device": device,
     }
 
-    # Equilateral triangle
+    # Square
     expected_vertices = spacing * torch.tensor(
         [
             [0.0, 0.0],
@@ -123,55 +170,120 @@ def test_square_qubo(traps: int, layout: LayoutType | str) -> None:
 
     result = Greedy().launch_greedy(Q=Q, params=parameters)
     vertices = result[0][1]["coords"]
+    print(f"\nvertices:\n{vertices}")
 
-    def sorted(vertices: torch.Tensor) -> torch.Tensor:
-        indices = np.lexsort(vertices.abs().numpy().T)
-        return vertices[indices,:].abs()
-
-    # Vertices form square of side = spacing
-    torch.testing.assert_close(sorted(vertices), sorted(expected_vertices))
+    assert_close_up_to_isometry(vertices, expected_vertices, torch.pi / 2.0)
+    # fmt: off
+    basis = spacing * torch.tensor(
+        [
+            [1.0, 0.0],
+            [0.0, 1.0],
+        ],
+        dtype=torch.float32,
+    ).T
+    # fmt: on
+    assert_close_to_lattice(vertices, basis)
 
     U = C6 * interaction_matrix_from_vertices(vertices)
     torch.testing.assert_close(U, expected_U)
 
-@pytest.mark.xfail(strict=False, reason="bug not fixed yet")
-def test_triangular_qubo_not_enough_traps() -> None:
 
-    spacing = 7.0
+@pytest.mark.parametrize("too_large", ["no", "barely", "extremely"])
+def test_too_large_spacing(too_large: str) -> None:
 
     device = DigitalAnalogDevice()._device
     C6 = device.interaction_coeff
 
+    # A square layout of size 25 is composed of two concentric squares of side
+    # 2*spacing and 4*spacing, plus the origin.
+    layout = LayoutType.SQUARE
+    traps = 25
+
+    # Only the origin is within the device's maximum radial distance
+    if too_large == "extremely":
+        spacing = device.max_radial_distance * 3.0
+    # Only the inner square is within the device's maximum radial distance
+    elif too_large == "barely":
+        spacing = device.max_radial_distance - 0.1
+    # All traps are within the device's maximum radial distance
+    else:
+        spacing = 7.0
+
     parameters = {
-        "layout": LayoutType.TRIANGULAR,
-        "traps": 2,
+        "layout": layout,
+        "traps": traps,
         "spacing": spacing,
         "device": device,
     }
 
-    # Equilateral triangle
+    # Tailored QUBO to match the below vertices
+    Q = torch.tensor(
+        [
+            [0.0, 1.0, 1.0 / 64.0],
+            [1.0, 0.0, 1.0 / 125.0],
+            [1.0 / 64.0, 1.0 / 125.0, 0.0],
+        ],
+        dtype=torch.float32,
+    )
+
+    # Tailored right triangle. With a correct spacing (e.g. 7.0):
+    #   - Vertex 0 is at the origin
+    #   - Vertex 1 is on the inner square
+    #   - Vertex 2 is on the outer square
     expected_vertices = spacing * torch.tensor(
         [
             [0.0, 0.0],
-            [0.5, 0.5 * np.sqrt(3)],
-            [1.0, 0.0],
+            [0.0, 1.0],
+            [2.0, 0.0],
         ]
     )
-    #  Matrix Q should match the spacing of the triangular layout so that the embedding returns
-    # an equilateral triangle, hence the scale alpha.
+    #  Matrix Q should match the spacing of the square layout so that the embedding returns
+    # an the expected right triangle, hence the scale alpha.
     expected_U = C6 * interaction_matrix_from_vertices(expected_vertices)
-    # All off-diagonal coefficients are equal to alpha
+    # The spacing corresponds to the distance between vertices 0 and 1
     alpha = expected_U[0, 1]
-    Q = alpha * triangular_qubo()
+
+    Q = alpha * Q
     torch.testing.assert_close(Q, expected_U)
 
-    result = Greedy().launch_greedy(Q=Q, params=parameters)
-    vertices = result[0][1]["coords"]
-    # Vertices form an equilateral triangle of side = spacing
-    for i in range(3):
-        for j in range(i + 1, 3):
-            d = torch.dist(vertices[i, :], vertices[j, :])
-            check.almost_equal(d, spacing)
+    greedy = Greedy()
 
+    if too_large == "extremely":
+        with pytest.raises(ValueError):
+            greedy.launch_greedy(Q=Q, params=parameters)
+        return
+
+    result = greedy.launch_greedy(Q=Q, params=parameters)
+    vertices = result[0][1]["coords"]
     U = C6 * interaction_matrix_from_vertices(vertices)
-    torch.testing.assert_close(U, expected_U)
+
+    # fmt: off
+    basis = spacing * torch.tensor(
+        [
+            [1.0, 0.0],
+            [0.0, 1.0],
+        ],
+        dtype=torch.float32,
+    ).T
+    # fmt: on
+    assert_close_to_lattice(vertices, basis)
+
+    if too_large == "no":
+        torch.testing.assert_close(U, expected_U)
+        assert_close_up_to_isometry(vertices, expected_vertices, torch.pi / 2.0)
+
+    elif too_large == "barely":
+        # expected_U is the interaction matrix if Q was perfectly embeddable
+        # in the square layout, with the given parameters, which is not the case here
+        expected_imperfect_U = expected_U
+        expected_imperfect_U[0, 2] = expected_imperfect_U[2, 0] = 1.0 / 8.0 * expected_U[0, 1]
+        torch.testing.assert_close(U, expected_imperfect_U)
+
+        expected_imperfect_vertices = spacing * torch.tensor(
+            [
+                [0.0, 0.0],
+                [1.0, 0.0],
+                [-1.0, 1.0],
+            ]
+        )
+        assert_close_up_to_isometry(vertices, expected_imperfect_vertices, torch.pi / 2.0)
