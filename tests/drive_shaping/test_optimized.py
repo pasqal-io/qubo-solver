97c78e1eb2a5a6f8c6924dbd981e791d2c62a695
Antoine Bussy <antoine.bussy@pasqal.com>
2026-03-12 16:58:54 +0100

    Add test triangular qubo

--- tests/drive_shaping/test_optimized.py
+++ tests/drive_shaping/test_optimized.py
@@ -5,16 +5,17 @@ import torch
 import itertools
 import numpy as np
 import math
+import pytest
+import pytest_check as check
+
 from qoolqit.devices.device import DigitalAnalogDevice
 from qoolqit.register import Register
+from qoolqit.execution import LocalEmulator
 
-from qubosolver.algorithms.greedy.greedy import Greedy
-from qubosolver.qubo_types import LayoutType
 from qubosolver.pipeline.drive import OptimizedDriveShaper
 from qubosolver.qubo_instance import QUBOInstance
 from qubosolver.config import SolverConfig, DriveShapingConfig
-import pytest
-import pytest_check as check
+from qubosolver.qubo_analyzer import QUBOAnalyzer
 
 
 def triangular_qubo() -> torch.Tensor:
@@ -70,7 +71,7 @@ def probability_based_ojective(
     min_cost = optimal_solutions[0].cost
     total_prob = sum(s.probability for s in optimal_solutions)
     # Weight in % of cost
-    w = math.copysign(0.1, min_cost)
+    w = math.copysign(0.5, min_cost)
     weighted_cost = min_cost * (1.0 + w * (1.0 - total_prob))
 
     optimal_bitstrings = [s.bitstring for s in optimal_solutions]
@@ -140,6 +141,73 @@ def test_equilateral_triangular_qubo(seed: int, use_probability_based_ojective: 
     if use_probability_based_ojective:
         total_optimal_probability = sum(s.probability for s in optimal_solutions)
         check.greater(total_optimal_probability, 0.35)
+
+    print(f"\nMinimum cost: {min_cost}")
+    print(f"All optimal bitstrings: {[s.bitstring for s in optimal_solutions]}")
+    print(f"Number of optimal solutions: {len(optimal_solutions)}\n")
+
+@pytest.mark.usefixtures("restore_rng_state")
+@pytest.mark.parametrize("seed", [ 412, 6983, 5674 ])
+@pytest.mark.parametrize("use_probability_based_ojective", [ True, False ])
+def test_triangular_qubo(seed: int, use_probability_based_ojective: bool) -> None:
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    spacing = 2.0
+
+    # Set a Register and compute the associated QUBO
+    # Equilateral triangle centered on origin
+    vertices = spacing * torch.tensor(
+        [
+            [0.0, 0.5],
+            [-0.8, -0.4],
+            [0.2, -0.15],
+        ],
+        dtype=torch.float32,
+    )
+    Q = 200.0 * (interaction_matrix_from_vertices(vertices) - 0.05 * torch.eye(3, dtype=torch.float32))
+
+    results = []
+    for bits in itertools.product([0, 1], repeat=3):
+        z = torch.tensor(bits, dtype=torch.float32)
+        cost = (z @ Q @ z).item()
+        results.append(Solution(''.join(str(int(b)) for b in z.flatten()), cost))
+
+    # Get all bitstrings with minimum cost
+    expected_optimal_solutions = gather_optimal_solutions(results)
+    check.is_not(expected_optimal_solutions, [])
+
+    print(f"\nExpected Minimum cost: {expected_optimal_solutions[0].cost}")
+    print(f"All expected optimal bitstrings: {[s.bitstring for s in expected_optimal_solutions]}")
+    print(f"Number of expected optimal solutions: {len(expected_optimal_solutions)}\n")
+
+    register = Register.from_coordinates(vertices.tolist())
+
+    ds_config = DriveShapingConfig()
+    if use_probability_based_ojective:
+        ds_config.optimized_custom_objective = probability_based_ojective
+    ds_config.optimized_n_calls = 11
+    config = SolverConfig(device=DigitalAnalogDevice(), drive_shaping=ds_config, backend=LocalEmulator(runs=500))
+
+    drive_shaper = OptimizedDriveShaper(QUBOInstance(Q), config, config.backend)
+    drive, solution = drive_shaper.generate(register)
+    solution.sort_by_cost()
+    analyzer = QUBOAnalyzer([solution])
+    print(f"{analyzer.df}")
+
+    optimal_solutions = gather_optimal_solutions(to_solutions(solution.bitstrings, solution.costs, solution.probabilities))
+    check.is_not(optimal_solutions, [])
+
+    min_cost = optimal_solutions[0].cost
+    check.almost_equal(min_cost, expected_optimal_solutions[0].cost)
+
+    expected_optimal_bistrings = [s.bitstring for s in expected_optimal_solutions]
+    for solution in optimal_solutions:
+        check.is_in(solution.bitstring, expected_optimal_bistrings)
+
+    total_optimal_probability = sum(s.probability for s in optimal_solutions)
+    check.greater(total_optimal_probability, 0.25)
 
     print(f"\nMinimum cost: {min_cost}")
     print(f"All optimal bitstrings: {[s.bitstring for s in optimal_solutions]}")
