5e934c945a952304d3bc4be64c65dbb4a72ad7f7
Quentin Ma <quentin.ma@pasqal.com>
2026-03-24 17:43:35 +0100

    remove division

--- tests/test_driveshaping.py
+++ tests/test_driveshaping.py
@@ -144,8 +144,7 @@ def test_normalized_weights_in_drive(
         norm_weights = list(wdetuning[0].weights.values())
         weights = torch.abs(torch.diag(simple_qubo_instance.coefficients)).tolist()
         max_w = max(weights)
-        TIME = default_config.device.converter.factors[0]
-        expected_norm = [(1 - (w / max_w)) / TIME for w in weights]
+        expected_norm = [(1 - (w / max_w)) for w in weights]
 
         assert pytest.approx(norm_weights, rel=1e-6) == expected_norm
         assert wdetuning[0].waveform.min() < 0
