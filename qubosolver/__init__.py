d7f3728e82c30466cc6536d27b41405db22f648b
abussy-pasqal <antoine.bussy@pasqal.com>
2026-06-03 14:35:45 +0200

    [Feature] Add metadata for cloud analytics (#171)

--- qubosolver/__init__.py
+++ qubosolver/__init__.py
@@ -8,9 +8,16 @@ from .qubo_types import *  # noqa: F403
 from .config import *  # noqa: F403
 from .utils import *  # noqa: F403
 
+from importlib.metadata import version
+from pulser.sequence import store_package_version_metadata
+
 list_of_submodules = [".data", ".utils", ".qubo_instance", ".config"]
 
 __all__ = []
 for submodule in list_of_submodules:
     __all_submodule__ = getattr(import_module(submodule, package="qubosolver"), "__all__")
     __all__ += __all_submodule__
+
+
+__version__ = version("qubo-solver")
+store_package_version_metadata("qubosolver", __version__)
