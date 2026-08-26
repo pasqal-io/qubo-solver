b343e4f3512d32f30f5b89349c3e16c219000b06
Antoine Bussy <antoine.bussy@pasqal.com>
2026-08-26 09:23:32 +0200

    wip

---
+++ qubosolver/solvers/hybrid/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from qubosolver.solvers.hybrid import (
+    drive_bayesian_search,
+)
+
+__all__ = [
+    "drive_bayesian_search",
+]
---
+++ qubosolver/solvers/quantum/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from qubosolver.solvers.quantum import (
+    analog_quantum_sampling,
+)
+
+__all__ = [
+    "analog_quantum_sampling",
+]
